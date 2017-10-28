4f1cabbb9e6496205232ce3bd1826c94eafdcb94
leticia <echo_hun@foxmail.com>
2017-10-28 12:06:42 +0800

    Delete 割线法.py

--- 割线法.py
+++
@@ -1,9 +0,0 @@
-import math
-while True:
-    x = input("x = ")
-    y = input("y = ")
-    for i in range(10):
-        z = y-(y*y*y-2*y-5)*(y-x)/(y*y*y-2*y-5-x*x*x+2*x+5)   #µü´ú¹«Ê½
-        print z
-        x = y
-        y = z
